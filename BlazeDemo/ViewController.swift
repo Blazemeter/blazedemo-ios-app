06fd5eb7a57afc965bee397221a58cb4b1d7bad4
Vijay J <vijayj@ca.com>
2017-12-18 09:37:56 -0800

    Initial Commit

---
+++ BlazeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlazeDemo
+//
+//  Created by Jambu, Vijay on 12/18/17.
+//  Copyright © 2017 Jambu, Vijay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
