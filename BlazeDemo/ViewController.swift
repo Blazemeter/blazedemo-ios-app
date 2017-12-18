0a375c6661050619d9f8fef247ef488d14e47c0f
Vijay J <vijayj@ca.com>
2017-12-18 15:17:25 -0800

    blazedemo app with a web view

--- BlazeDemo/ViewController.swift
+++ BlazeDemo/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import WebKit
+import os.log
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url:URL? = URL(string: "http://www.blazedemo.com/")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        os_log("navigation finished for webview %@",webView.url!.absoluteString)
+        print("navigation finished for webview \(webView.url!.absoluteString)")
+        title = webView.title
+    }
 
 }
 
